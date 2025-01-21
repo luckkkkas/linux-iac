85a5bdbf8d7732e9dab19efca015ab72720a7996
Lucas Oliveira <lucashbo321@gmail.com>
2025-01-21 19:43:25 +0000

    Teste no linux

--- ServerConfiguration_Docker.sh
+++ ServerConfiguration_Docker.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-# atualizando servidor
+# Atualizando o servidor
 apt update -y
 apt upgrade -y
 
-# instalando pacotes necessarios
-apt install unzip -y
-apt install apache2 -y
-apt install openssh-server -y
+# Instalando pacotes necessários
+apt install unzip apache2 openssh-server -y
 
-# instalando docker
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do  apt-get remove $pkg; done
+# Removendo pacotes conflitantes relacionados ao Docker
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do  
+  apt-get remove -y $pkg
+done
 
-# Add Docker's official GPG key:
+# Adicionando o GPG oficial do Docker
 apt-get update -y
 apt-get install ca-certificates curl -y
-install -m 0755 -d /etc/apt/keyrings -y
+install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
+# Adicionando o repositório Docker à lista de fontes do Apt
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-   tee /etc/apt/sources.list.d/docker.list > /dev/null
- apt-get update -y
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+# Atualizando repositórios e instalando o Docker
+apt-get update -y
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
